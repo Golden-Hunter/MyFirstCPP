e1e52b81df5e8552aca06b542ca2a49ab0b14bfa
X1Warrior <zsto7638@gmail.com>
2018-01-23 12:45:59 -0700

    added my class file.

---
+++ MyFirstCPP/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MyFirstCPP
+//
+//  Created by Stout, Zachary on 1/23/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MyFirstCPP/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  MyFirstCPP
+//
+//  Created by Stout, Zachary on 1/23/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
