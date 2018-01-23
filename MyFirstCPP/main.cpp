8f2381200e750bd77cd6a2065d61809dc9aaa8fd
X1Warrior <zsto7638@gmail.com>
2018-01-23 12:30:32 -0700

    GitIgnore c++ as well as made a basic hello world.

---
+++ MyFirstCPP/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  MyFirstCPP
+//
+//  Created by Stout, Zachary on 1/23/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!\n" << endl;
+    string input;
+    getline(cin, input);
+    cout << "You said: " << input << endl;
+    return 0;
+}
