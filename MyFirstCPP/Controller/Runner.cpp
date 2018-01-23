ec1dadbda26be757ae1bc1c0d26a2a864abc0d8e
X1Warrior <zsto7638@gmail.com>
2018-01-23 12:38:10 -0700

    created a runner file and code sucessfull does nothing.

---
+++ MyFirstCPP/Controller/Runner.cpp
@@ -0,0 +1,13 @@
+//
+//  Runner.cpp
+//  MyFirstCPP
+//
+//  Created by Stout, Zachary on 1/23/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+int main()
+{
+    
+    return 0;
+}
